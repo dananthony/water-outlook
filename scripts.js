51b6188d925a56807cf422ec2aaf5699928c2c60
Dan Anthony <dan@paperfox.com.au>
2020-11-26 11:14:30 +0100

    refactor scripts

--- scripts.js
+++ scripts.js
@@ -1,14 +1,63 @@
 jQuery(function($) {
 	$('#urban').addClass('active');
 
+	// update the footer and sidebar
+	function updateFooter(contentType) {
+		if (contentType.indexOf('environmental') > -1) {
+			$('.gs-footer__wrapper[data-type="environmental"]').show();
+			$('.gs-footer__wrapper[data-type="urban-rural"]').hide();
+		} else {
+			$('.gs-footer__wrapper[data-type="environmental"]').hide();
+			$('.gs-footer__wrapper[data-type="urban-rural"]').show();
+		}
+	}
+
+	function updateLegend(contentType, subTitle) {
+		// update subtitle
+		$('.gs-header__legend-subtitle span').text(subTitle);
+		
+		// update text 
+		$('.gs-header__legend-text').removeClass('active');
+		$('.gs-header__legend-text[data-type='+contentType+']').addClass('active');
+
+		// toggle icon and subtitle visibility 
+		if (contentType.indexOf('urban') > -1) {
+			$('.gs-header__legend-key').css('visibility', 'visible');
+			$('.gs-header__legend-subtitle').css('visibility', 'visible');
+		}
+
+		if (contentType.indexOf('rural') > -1) {
+			$('.gs-header__legend-key').css('visibility', 'hidden');
+			$('.gs-header__legend-subtitle').css('visibility', 'visible');
+		}
+		if (contentType.indexOf('environmental') > -1) {
+			$('.gs-header__legend-key').css('visibility', 'hidden');
+			$('.gs-header__legend-subtitle').css('visibility', 'hidden');
+		}
+	}
+
+	function updateMapColor(climateScenario) {
+		var color = "#fff"
+		if (climateScenario === 'dry') {
+			color = "#ffffcc";
+		}
+		if (climateScenario === 'worst') {
+			color = "#ffcc99";
+		}
+		$('#map').css('fill', color);
+	}
+
 	// activate buttons
 	$('.gs-header__btn').on('click', function() {
 	
 		var useType = $(this).data('use');
+		var buttonTitle = $(this).text();
 		var contentType = $(this).data('type');
+		var climateScenario = $(this).data('climate');
+
 		var isUseButton = $(this).data('use') !== undefined;
 		var parentUseActive = $(this).parent().prev().find('.gs-header__btn').hasClass('active');
-
+		
 		if (isUseButton) {
 			// update active use button
 			$('.gs-header__btn').removeClass('active');
@@ -19,32 +68,17 @@ jQuery(function($) {
 			$('#layers').children().removeClass('active');
 			$(mapLayer).addClass('active');
 
-			// set default climate scenario
+			// set default climate scenario and map color
 			$(this).parent().next().find('.gs-header__btn').first().addClass('active');
 			$('#map').css('fill', '#fff');
 
-			// update legend title
-			$('.gs-header__legend-title span').text($(this).text());
+			// update legend title 
+			$('.gs-header__legend-title span').text(buttonTitle);
 
-			// set default legend text
-			$('.gs-header__legend-text').removeClass('active');
-			$('.gs-header__legend-text[data-type='+contentType+']').addClass('active');
-
-			// show/hide legend sections
-			if (contentType.indexOf('urban') > -1) {
-				$('.gs-header__legend-key').css('visibility', 'visible');
-				$('.gs-header__legend-subtitle').css('visibility', 'visible');
-			}
-
-			if (contentType.indexOf('rural') > -1) {
-				$('.gs-header__legend-key').css('visibility', 'hidden');
-				$('.gs-header__legend-subtitle').css('visibility', 'visible');
-			}
-			if (contentType.indexOf('environmental') > -1) {
-				$('.gs-header__legend-key').css('visibility', 'hidden');
-				$('.gs-header__legend-subtitle').css('visibility', 'hidden');
-			}
-
+			// show/hide legend and footer sections
+			updateLegend(contentType, buttonTitle);
+			updateFooter(contentType);
+			
 		} else {
 			if (parentUseActive) {
 				// update active climate scenario button
@@ -52,37 +86,10 @@ jQuery(function($) {
 				$(this).addClass('active');
 
 				// update base map color
-				var climateScenario = $(this).data('climate');
-				var color = "#fff"
-				if (climateScenario === 'dry') {
-					color = "#ffffcc";
-				}
-				if (climateScenario === 'worst') {
-					color = "#ffcc99";
-				}
-				$('#map').css('fill', color);
+				updateMapColor(climateScenario);
 
-				// update legend subtitle
-				$('.gs-header__legend-subtitle span').text($(this).text());
-
-				// update legend text
-				$('.gs-header__legend-text').removeClass('active');
-				$('.gs-header__legend-text[data-type='+contentType+']').addClass('active');
-
-				// show/hide legend sections
-				if (contentType.indexOf('urban') > -1) {
-					$('.gs-header__legend-key').css('visibility', 'visible');
-					$('.gs-header__legend-subtitle').css('visibility', 'visible');
-				}
-
-				if (contentType.indexOf('rural') > -1) {
-					$('.gs-header__legend-key').css('visibility', 'hidden');
-					$('.gs-header__legend-subtitle').css('visibility', 'visible');
-				}
-				if (contentType.indexOf('environmental') > -1) {
-					$('.gs-header__legend-key').css('visibility', 'hidden');
-					$('.gs-header__legend-subtitle').css('visibility', 'hidden');
-				}
+				// update legend 
+				updateLegend(contentType, buttonTitle);
 
 
 			} else {
@@ -98,36 +105,14 @@ jQuery(function($) {
 				$(mapLayer).addClass('active');
 
 				// update base map colour
-				var climateScenario = $(this).data('climate');
-				var color = "#fff"
-				if (climateScenario === 'dry') {
-					color = "#ffffcc";
-				}
-				if (climateScenario === 'worst') {
-					color = "#ffcc99";
-				}
-				$('#map').css('fill', color);
+				updateMapColor(climateScenario);
 
 				// update legend title
 				$('.gs-header__legend-title span').text($(this).parent().prev().find('.gs-header__btn').text());
 
-				// update legend subtitle
-				$('.gs-header__legend-subtitle span').text($(this).text());
-
 				// show/hide legend sections
-				if (contentType.indexOf('urban') > -1) {
-					$('.gs-header__legend-key').css('visibility', 'visible');
-					$('.gs-header__legend-subtitle').css('visibility', 'visible');
-				}
-
-				if (contentType.indexOf('rural') > -1) {
-					$('.gs-header__legend-key').css('visibility', 'hidden');
-					$('.gs-header__legend-subtitle').css('visibility', 'visible');
-				}
-				if (contentType.indexOf('environmental') > -1) {
-					$('.gs-header__legend-key').css('visibility', 'hidden');
-					$('.gs-header__legend-subtitle').css('visibility', 'hidden');
-				}
+				updateLegend(contentType, buttonTitle);
+				updateFooter(contentType);
 			}
 		}
 	})
